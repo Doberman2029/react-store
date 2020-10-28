6a3bb528ef3f81f986ad4c2e52cc69b32dd78b24
Doberman2029 <yakushina20@mail.ru>
2020-10-28 15:07:32 +0300

    Updates

---
+++ precache-manifest.db2ca6cce552d0bc90f70eeae19bd053.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a723bf1ff07f47fd0080e8fc998d84",
+    "url": "/react-store/index.html"
+  },
+  {
+    "revision": "248ca152c90cd9c25e7f",
+    "url": "/react-store/static/css/main.b218869a.chunk.css"
+  },
+  {
+    "revision": "57aa4f97217160321c87",
+    "url": "/react-store/static/js/2.e6b2186c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-store/static/js/2.e6b2186c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248ca152c90cd9c25e7f",
+    "url": "/react-store/static/js/main.e3e5bbaa.chunk.js"
+  },
+  {
+    "revision": "4a821b21230fe3b6f0db",
+    "url": "/react-store/static/js/runtime-main.3258afb6.js"
+  }
+]);
